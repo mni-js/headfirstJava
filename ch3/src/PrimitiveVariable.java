6208bcdfdd4cded4ae04987caa0ca257a776d460
jse <2ujs4311@gmail.com>
2025-02-04 19:07:12 +0900

    형변환 가능, 불가능 테스트

---
+++ ch3/src/PrimitiveVariable.java
@@ -0,0 +1,87 @@
+public class PrimitiveVariable {
+    public static void main(String[] args) {
+        // char
+        // char c = 'A';
+
+        // char -> byte: X
+        // byte b = c;
+
+        // char -> short: X
+        // short s = c;
+        
+        // char -> int: O 
+        // int i = c;
+
+        // char -> float: O
+        // float f = c;
+        
+        // byte
+        // byte b = 127;
+
+        // byte -> char: X
+        // char c = b;
+
+        // byte -> short: O
+        // short s = b;
+
+        // byte -> float: O
+        // float f = b;
+
+        // byte -> double: O 
+        // double d = b;
+
+        // short
+        // short s = 1;
+
+        // short -> char: X
+        // char c = s;
+
+        // short -> byte: X
+        // byte b = s;
+
+        // float
+        // float f = 3.2F;
+
+        // float -> char: X
+        // char c = f;
+
+        // float -> byte: X
+        // byte b = f;
+
+        // float -> short: X
+        // short s = f;
+
+        // float -> int: X
+        // int i = f;
+
+        // float -> long: X
+        // long l = f;
+
+        // float -> double: O
+        // double d = f;
+
+        // double
+        // double d = 1.0;
+
+        // double -> int: X
+        // int i = d;
+
+        // double -> float: X
+        // float f = d;
+
+        // boolean
+        boolean t = true;
+
+        // boolean -> char: X
+        // char c = t;
+
+        // boolean -> int: X
+        // int i = t;
+
+        // boolean -> double: X 
+        // double d = t;
+        System.out.println(d);
+
+
+    }
+}
