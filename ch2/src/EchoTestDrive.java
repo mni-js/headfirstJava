386db267b4adb9d1e042a9d76b2f2d4de961ffa4
jse <2ujs4311@gmail.com>
2025-02-04 20:54:00 +0900

    챕터 2 공부 완료

--- ch2/src/EchoTestDrive.java
+++ ch2/src/EchoTestDrive.java
@@ -10,7 +10,7 @@ public class EchoTestDrive {
     public static void main(String[] args) {
         Echo e1 = new Echo();
         Echo e2 = new Echo();
-        int x = 0;
+        int x = 0; 
 
         while (x < 4) {
             e1.hello();
