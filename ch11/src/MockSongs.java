426f906d4d1da9e971ff53e181b26436a53c4509
jse <2ujs4311@gmail.com>
2025-02-15 13:03:57 +0900

    Collections.sort()에 대해 공부

---
+++ ch11/src/MockSongs.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class MockSongs {
+    public static List<String> getSongStrings() {
+        List<String> songs = new ArrayList<>();
+        songs.add("somersault");
+        songs.add("cassidy");
+        songs.add("$10");
+        songs.add("havana");
+        songs.add("Cassidy");
+        songs.add("50 Ways");
+
+        return songs;
+    }
+
+    public static List<SongV2> getSongsV2() {
+        List<SongV2> songs = new ArrayList<>();
+        songs.add(new SongV2("somersault", "zero 7", 147));
+        songs.add(new SongV2("cassidy", "grateful dead", 158));
+        songs.add(new SongV2("$10", "hitchhiker", 140));
+
+        songs.add(new SongV2("havana", "cabello", 105));
+        songs.add(new SongV2("Cassidy", "grateful dead", 158));
+        songs.add(new SongV2("50 Ways", "simon", 102));
+
+        return songs;
+    }
+}
